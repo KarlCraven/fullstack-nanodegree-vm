9909b591ade679553f165c916ad194891e60d47c
Karl Craven <kandrewcraven@gmail.com>
2015-12-10 19:00:12 -0500

    Removed insetion of dummy table rows from tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -12,26 +12,6 @@ CREATE TABLE players (
     id      serial PRIMARY KEY,
     name    text
 );
-
-
--- Populate players table with demo data
-INSERT INTO players (id, name) VALUES
-    (DEFAULT, 'Bill'),
-    (DEFAULT, 'Bob'),
-    (DEFAULT, 'Jim'),
-    (DEFAULT, 'Susan'),
-    (DEFAULT, 'Anne'),
-    (DEFAULT, 'Victoria'),
-    (DEFAULT, 'William'),
-    (DEFAULT, 'Anneke'),
-    (DEFAULT, 'John'),
-    (DEFAULT, 'Ian'),
-    (DEFAULT, 'Alice'),
-    (DEFAULT, 'Jamie'),
-    (DEFAULT, 'George'),
-    (DEFAULT, 'Sam'),
-    (DEFAULT, 'Lee'),
-    (DEFAULT, 'Cat');
     
 
 -- Create tournaments table 
@@ -39,6 +19,7 @@ CREATE TABLE tournaments (
     id      serial PRIMARY KEY,
     name    text
 );
+
 
 -- Populate tournaments table with demo data
 INSERT INTO tournaments (id, name) VALUES
@@ -53,25 +34,6 @@ CREATE TABLE competitors (
     PRIMARY KEY (tournament_id, competitor_id)
 );
 
--- Populate competitors table with demo data
-INSERT INTO competitors (tournament_id, competitor_id) VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (1, 4),
-    (1, 5),
-    (1, 6),
-    (1, 7),
-    (1, 8),
-    (2, 9),
-    (2, 10),
-    (2, 11),
-    (2, 12),
-    (2, 13),
-    (2, 14),
-    (2, 15),
-    (2, 16);
-
 
 -- Create matches table    
 CREATE TABLE matches (
@@ -81,10 +43,3 @@ CREATE TABLE matches (
     winner_id       integer REFERENCES players(id),
     PRIMARY KEY (tournament_id, player_1_id, player_2_id)
 );
-
--- Populate matches table with demo data
---INSERT INTO matches (player_1_id, player_2_id, winner_id) VALUES
---    (1, 2, 1),
---    (3, 4, 3),
---    (5, 6, 6),
---    (7, 8, 7);
